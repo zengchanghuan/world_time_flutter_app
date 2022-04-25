a650cb8297dc1f00412069e5cb63781622fb76d2
zengchanghuan <zengchanghuan@gmail.com>
2022-04-25 14:13:53 +0800

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
-import 'package:flutter/material.dart';
-import 'pages/home.dart';
-void main() {
-  runApp(MaterialApp());
-  home:Home();
-}
+import 'dart:js';
 
+import 'package:flutter/material.dart';
+import 'package:world_time_flutter_app/pages/home.dart';
+import 'package:world_time_flutter_app/pages/loading.dart';
+import 'package:world_time_flutter_app/pages/choose_location.dart';
+
+void main () => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes:{
+    '/':(context) => Loading(),
+    '/home':(context) => Home(),
+    '/location':(context) => ChooseLocation(),
+  },
+));
